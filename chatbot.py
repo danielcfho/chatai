762bc42e867fb195a6e7d9a4c15709757f92ab09
Daniel Ho <danielho@hko.hksarg>
2023-03-03 11:40:03 +0800

    add chatbot use gpt-3.5-turbo model

---
+++ chatbot.py
@@ -0,0 +1,24 @@
+#%%
+from dotenv import load_dotenv
+import os
+import openai
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+print(openai.api_key)
+
+#%%
+messages = []
+system_msg = input("what type of chatbot would you like to use?\n")
+messages.append({"role":"system","content":system_msg})
+
+print("Say hello")
+while ( message := input('') ) != 'quit()':
+  messages.append({"role":"user","content":message})
+  response = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo",
+    messages=messages)
+  reply = response["choices"][0]["message"]["content"]
+  messages.append({"role":"user","content":reply})
+  print("\n" + reply + "\n")
