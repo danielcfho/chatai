b20724fbb889d8153fbd6225152c5a42238754c3
Daniel Ho <daniel.cf.ho@gmail.com>
2023-03-03 14:46:33 +0800

    add english translator mode

--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,16 @@
-#%%
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 import openai
 load_dotenv()
 
+PROMPT_TEACH_ENG='''
+I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to write explanations. My first sentence is "你好嗎？"'''
+
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-print(openai.api_key)
-
-#%%
 messages = []
-system_msg = input("what type of chatbot would you like to use?\n")
+# system_msg = input("what type of chatbot would you like to use?\n")
+system_msg = PROMPT_TEACH_ENG
 messages.append({"role":"system","content":system_msg})
 
 print("Say hello")
